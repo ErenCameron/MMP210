8743eccaa9ca1074e164e795c7b776b55cf96278
Eren <erencameron@gmail.com>
2019-09-12 15:43:43 -0400

    began drawing sketch, half of face done

--- self_portrait/sketch.js
+++ self_portrait/sketch.js
@@ -1,16 +1,26 @@
 function setup() {
-	createCanvas(640, 360);
+	createCanvas(500, 500);
 }
 
-
+//face is drawn clockwise from top midpoint of head
 function draw() {
-	background(0);
-	ellipse(320, 180, 100);
-	
-	fill("LAVENDER");
-	rect(100,100,100,100);
-	arc(400,100,100,100,1,5.5);
-
+	background(255);
+	line(250,30,340,40) //1
+	line(340,40,400,70) //2
+	line(400,70,400,130) //3
+	beginShape(TRIANGLE_STRIP); //right ear "4"
+	vertex(380,180);
+	vertex(400,130);
+	vertex(420,100);
+	vertex(450,120);
+	vertex(450,150);
+	vertex(400,220);
+	vertex(420,170);
+    endShape(CLOSE);
+    line(400,220,400,350) //5
+    line(400,350,325,380) //6
+    line(325,380,175,380) //7
+    line(175,380,100,350)
 }
 
 // this is a comment

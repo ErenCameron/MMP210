e146c8e9a475342ece368ddec15530d40443b9f3
eren <erencameron@gmail.com>
2019-09-17 15:10:55 -0400

    changed vertices to be variables in shapes

--- variables/sketch.js
+++ variables/sketch.js
@@ -1,5 +1,10 @@
 // with variables 9/17
 
+
+var mouthSize = 70;
+var x = 250;
+var y= 30;
+
 function setup() {
 	createCanvas(500, 500);
 }
@@ -7,7 +12,7 @@ function setup() {
 function draw() {
 	background(255);
 
-	fill(255,175,100)
+	fill(x+5,x-75,x-150)
     stroke(0)
  //    beginShape(QUAD_STRIP);  //face commenting out for now may look to reintegrate
 	// vertex(250,30) 
@@ -25,36 +30,36 @@ function draw() {
  //    vertex(250,30) 
  //    endShape(CLOSE);
     beginShape(QUADS);  //face 2
-    vertex(250,30) 
-    vertex(340,40)
-    vertex(400,70) 
-    vertex(400,130) 
-    vertex(400,220) 
-    vertex(400,350) 
-    vertex(325,380) 
-    vertex(175,380) 
-    vertex(100,350) 
-    vertex(100,130) 
-    vertex(100,70) 
-    vertex(160,40)
-    vertex(250,30) 
+    vertex(x,y) 
+    vertex(x+90,y+10)
+    vertex(x+150,y+40) 
+    vertex(x+150,y+100) 
+    vertex(x+150,y+190) 
+    vertex(x+150,y+320) 
+    vertex(x+75,y+350) 
+    vertex(x-75,y+350) 
+    vertex(x-150,y+320) 
+    vertex(x-150,y+100) 
+    vertex(x-150,y+40) 
+    vertex(x-90,y+10) 
+    vertex(x,y) 
     endShape(CLOSE);
 
-    fill(119,170,54);
+    fill(x-131,x-80,x-196);
 
     beginShape(QUADS);
-    vertex(200,80)  //beginning of left eye
-    vertex(220,100) 
-    vertex(220,120) 
-    vertex(200,140) 
-    vertex(180,140) 
-    vertex(160,120) 
-    vertex(160,100) 
-    vertex(180,80)
-    vertex(200,80) 
+    vertex(x-50,y+50)  //beginning of left eye
+    vertex(x-30,y+70) 
+    vertex(x-30,y+90) 
+    vertex(x-50,y+110) 
+    vertex(x-70,y+110) 
+    vertex(x-90,y+90) 
+    vertex(x-90,y+70) 
+    vertex(x-70,y+50)
+    vertex(x-50,y+50) 
     endShape(CLOSE); // end of left 
 
-    fill(0,255,255)
+    fill(x-250,x+5,x+5)
 
     beginShape();
     vertex(320,80) //22 beginning of right eye
@@ -67,7 +72,7 @@ function draw() {
     vertex(300,80)
     vertex(320,80) //28 end of right eye
     endShape(CLOSE);
-fill   (255,255,100);
+fill   (x+5,x+5,x-150);
 
     beginShape(TRIANGLE_STRIP); //right ear "4" clockwise from left most point on ear
     vertex(380,180); 
@@ -89,19 +94,19 @@ fill   (255,255,100);
     endShape(CLOSE);
     
     stroke(0)
-    fill(150,20,50)
+    fill(x-100,y-10,x+150)
 
     beginShape(QUAD_STRIP) // nose
-    vertex(250,170);
-    vertex(270,200);
-    vertex(250,230);
-    vertex(230,200);
-    vertex(250,170);
+    vertex(250,y+140);
+    vertex(270,y+170);
+    vertex(250,y+200);
+    vertex(230,y+170);
+    vertex(250,y+140);
     endShape(CLOSE); // end nose
 
-    fill(0,0,255)
+    fill(x-250,x-250,x+5)
 
-    circle(250,280,70); //mouth
+    circle(250,280,mouthSize); //mouth
    
 }
 

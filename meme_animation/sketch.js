26d13d690b8ee915282a82d2b26dcfc65cb734bd
Eren <erencameron@gmail.com>
2019-10-22 12:54:02 -0400

    added functionality

--- meme_animation/sketch.js
+++ meme_animation/sketch.js
@@ -1,20 +1,28 @@
 function setup() {
 	createCanvas(500, 500);
 }
-var wordPosition = 100;
-var wordSpeed = 10;
+var wordPosition = 100; //x position of the text
+var wordSpeed = 10; //direction and speed of text
+var wordSize = 50; //size of text
+var wordPositionY = 200; //y position of text
 
 function draw() {
     
 	background(255);
-    textSize (50);
-    text ("okay boomer", wordPosition, 200);
+    textSize (wordSize);
+    text ("okay boomer", wordPosition, wordPositionY); 
     wordPosition += wordSpeed;
-    if (wordPosition > width || wordPosition <-150) {
+    if (wordPosition > width || wordPosition <-300) { //moves text from right to left 
         wordSpeed *= -1;
-    }
-	
+    }                                                 //and changes direction when hits side
     
+	if (mouseIsPressed) {
+        wordSize = wordSize + 5;  //changes size of the text and
+        if (wordSize > 200) {
+            wordSize = 0;
+        }
+    }
+
 }
 
 	

5589f33d97e8dc8abe611f82cf06a452c04f0abc
eren <erencameron@gmail.com>
2019-11-14 15:15:14 -0500

    updated behavior of sketch

--- sensor/sketch.js
+++ sensor/sketch.js
@@ -1,5 +1,5 @@
 var serial;
-var portName = "COM22";
+var portName = "COM17";
 var xaxis, yaxis;
 
 function setup() {
@@ -34,17 +34,20 @@ function serialEvent(){
 		return;
 	}
 //	sensorValue = currentString;
+    
 	xaxis = currentString.split(',')[0]; 
 	yaxis = currentString.split(',')[1];
 }
 function draw() {
 	var xRect = map(xaxis, 0, 1023, 0, 255);
 	var yRect = map(yaxis, 0, 1023, 0, 255);
-	background(255);
-	rect(xRect, yRect, 100, 100);
-	rect(xRect + 100, yRect, 100, 100);
-	rect(xRect - 100, yRect + 100, 100, 100);
-	rect(xRect + 100, yRect - 100, 100, 100);
+	background(xRect, yRect, 60);
+    fill('white');
+	ellipse(xRect, yRect, xRect+100, yRect+100);
+	ellipse(xRect + 100, yRect, 100, 100);
+	ellipse(xRect - 100, yRect + 100, 100, 100);
+	ellipse(xRect + 100, yRect - 100, 100, 100);
+    ellipse(300, 300, xRect, yRect);
 	
 	
 }
